246541b5e56e91e05e3ca8810e104ccdbbb82d75
Netfloex <netfloex@gmail.com>
2021-04-30 16:11:15 +0200

    Basic Nextron App

---
+++ renderer/next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	future: {
+		webpack5: true,
+		strictPostcssConfiguration: true,
+	},
+	reactStrictMode: true,
+	webpack: (config) =>
+		Object.assign(config, {
+			target: "electron-renderer",
+		}),
+};
---
+++ renderer/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	mode: "jit",
+	purge: ["./renderer/components/**/*.{js,ts,jsx,tsx}", "./renderer/pages/**/*.{js,ts,jsx,tsx}"],
+	darkMode: false,
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
